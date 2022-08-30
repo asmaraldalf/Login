bf20952f800087594e9d5c3c8ed1bdee0c44684e
Asmaralda Laksmi Firdausi <asmaraldalf@gmail.com>
2022-08-30 20:38:11 +0700

    initial commit

---
+++ Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace LoginClient
+{
+    class Client
+    {
+        static void Main()
+        {
+            Person person = new Person();
+
+            Console.WriteLine("username: ");
+            person.Username = Console.ReadLine();
+            Console.WriteLine("password: ");
+            person.Password = Console.ReadLine();
+
+            string fileName = "UserData.json";
+            string jsonString = JsonSerializer.Serialize(person);
+            File.WriteAllText(fileName, jsonString);
+
+            Console.WriteLine(File.ReadAllText(fileName));
+        }
+    }
+
+    class Person
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
